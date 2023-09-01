c0c46912b7a4c621f5743bbae917c7faf1bed022
Stepaniuk-Den <stepaniuk.den@gmail.com>
2023-09-01 11:41:07 +0300

    update CreatePostsScreen

--- src/Screens/CreatePostsScreen.jsx
+++ src/Screens/CreatePostsScreen.jsx
@@ -6,61 +6,132 @@ import {
   Pressable,
   TouchableWithoutFeedback,
   Keyboard,
+  TouchableOpacity,
 } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import CustomInput from "../components/CustomInput/CustomInput";
 import CustomButton from "../components/CustomButton/CustomButton";
+import CustomCamera from "../components/CustomCamera/CustomCamera";
 
 import { Feather } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
 
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+
 const CreatePostsScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [cameraActivated, setCameraActivated] = useState(false);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      await MediaLibrary.requestPermissionsAsync();
+
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.root}>
-        <View style={styles.imageThumb}>
-          <Image
-            style={styles.image}
-            source={require("../../assets/images/testForest.png")}
-          />
-          <View style={styles.imageCamera}>
-            <FontAwesome name="camera" size={24} color="#bdbdbd" />
+    //
+    <>
+      {cameraActivated ? (
+        // <CustomCamera />
+        <View style={styles.container}>
+          <Camera style={styles.camera} type={type} ref={setCameraRef}>
+            <View style={styles.photoView}>
+              <TouchableOpacity
+                style={styles.flipContainer}
+                onPress={() => {
+                  setType(
+                    type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back
+                  );
+                }}
+              >
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: "white" }}
+                >
+                  {" "}
+                  Flip{" "}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={async () => {
+                  setCameraActivated(false);
+                  if (cameraRef) {
+                    const { uri } = await cameraRef.takePictureAsync();
+                    await MediaLibrary.createAssetAsync(uri);
+                    console.warn(uri);
+                  }
+                }}
+              >
+                <View style={styles.takePhotoOut}>
+                  <View style={styles.takePhotoInner}></View>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      ) : (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.root}>
+            <View style={styles.imageThumb}>
+              {/* <Image style={styles.image} source={uri} /> */}
+              <Pressable
+                style={styles.imageCamera}
+                onPress={() => setCameraActivated(true)}
+              >
+                <FontAwesome name="camera" size={24} color="#bdbdbd" />
+              </Pressable>
+            </View>
+            <Text style={styles.imageText}>Завантажте фото</Text>
+            <CustomInput
+              name="title"
+              placeholder="Назва..."
+              placeholderTextColor="#bdbdbd"
+              // value={title}
+              // onChangeText={handleChange("title")}
+              // onChangeText={(value)=> setTitle(value)}
+              type="INACTIVE"
+            />
+            <View style={styles.location}>
+              <CustomInput
+                name="location"
+                placeholder="Місцевість..."
+                placeholderTextColor="#bdbdbd"
+                // value={location}
+                // onChangeText={handleChange("location")}
+                type="INACTIVE_MAP"
+              />
+              <Pressable style={styles.locationIcon}>
+                <Feather name="map-pin" size={24} color="#BDBDBD" />
+              </Pressable>
+            </View>
+            <CustomButton
+              text="Опубліковати"
+              // onPress={}
+              type="INACTIVE"
+            />
+            <View style={styles.iconTrash}>
+              <Feather name="trash-2" size={24} color="#BDBDBD" />
+            </View>
           </View>
-        </View>
-        <Text style={styles.imageText}>Завантажте фото</Text>
-        <CustomInput
-          name="title"
-          placeholder="Назва..."
-          placeholderTextColor="#bdbdbd"
-          // value={title}
-          // onChangeText={handleChange("title")}
-          // onChangeText={(value)=> setTitle(value)}
-          type="INACTIVE"
-        />
-        <View style={styles.location}>
-          <CustomInput
-            name="location"
-            placeholder="Місцевість..."
-            placeholderTextColor="#bdbdbd"
-            // value={location}
-            // onChangeText={handleChange("location")}
-            type="INACTIVE_MAP"
-          />
-          <Pressable style={styles.locationIcon}>
-            <Feather name="map-pin" size={24} color="#BDBDBD" />
-          </Pressable>
-        </View>
-        <CustomButton
-          text="Опубліковати"
-          // onPress={}
-          type="INACTIVE"
-        />
-        <View style={styles.iconTrash}>
-          <Feather name="trash-2" size={24} color="#BDBDBD" />
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
+        </TouchableWithoutFeedback>
+      )}
+    </>
   );
 };
 
@@ -125,6 +196,41 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: "45%",
     left: 0,
+  },
+  container: { flex: 1 },
+  camera: { flex: 1 },
+  photoView: {
+    flex: 1,
+    backgroundColor: "transparent",
+    justifyContent: "flex-end",
+  },
+
+  flipContainer: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+  },
+
+  button: { alignSelf: "center" },
+
+  takePhotoOut: {
+    borderWidth: 2,
+    borderColor: "white",
+    height: 50,
+    width: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    marginBottom: 40,
+  },
+
+  takePhotoInner: {
+    borderWidth: 2,
+    borderColor: "white",
+    height: 40,
+    width: 40,
+    backgroundColor: "white",
+    borderRadius: 50,
   },
 });
 
---
+++ src/components/CustomCamera/CustomCamera.jsx
@@ -0,0 +1,106 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+
+const CustomCamera = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      await MediaLibrary.requestPermissionsAsync();
+
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type} ref={setCameraRef}>
+        <View style={styles.photoView}>
+          <TouchableOpacity
+            style={styles.flipContainer}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+              {" "}
+              Flip{" "}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={async () => {
+              if (cameraRef) {
+                const { uri } = await cameraRef.takePictureAsync();
+                await MediaLibrary.createAssetAsync(uri);
+              }
+            }}
+          >
+            <View style={styles.takePhotoOut}>
+              <View style={styles.takePhotoInner}></View>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+  },
+  camera: { flex: 1 },
+  photoView: {
+    flex: 1,
+    backgroundColor: "transparent",
+    justifyContent: "flex-end",
+  },
+
+  flipContainer: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+  },
+
+  button: { alignSelf: "center" },
+
+  takePhotoOut: {
+    borderWidth: 2,
+    borderColor: "white",
+    height: 50,
+    width: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+  },
+
+  takePhotoInner: {
+    borderWidth: 2,
+    borderColor: "white",
+    height: 40,
+    width: 40,
+    backgroundColor: "white",
+    borderRadius: 50,
+  },
+});
+
+export default CustomCamera;
